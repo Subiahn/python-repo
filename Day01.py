a15ec6f6c4c50b6bf0185277fce102095eb813b9
Subiahn <130112343+Subiahn@users.noreply.github.com>
2024-01-03 15:39:11 +0900

    밴드 이름 만들이

---
+++ Day01.py
@@ -0,0 +1,9 @@
+print("Welcom to the Band Name Generator\n")
+
+
+city = input("what is name of the city your grew up in?\n")
+
+pet_name = input("what is your pet name\n")
+
+
+print("Your Band Name could be "+ city + " " + pet_name)
