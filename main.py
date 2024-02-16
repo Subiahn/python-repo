53be3643145b66e547ad820ebfce9d0f69458d33
Subiahn <130112343+Subiahn@users.noreply.github.com>
2024-02-16 23:33:40 +0900

    Day_20_snake_game_1
    뱀 게임 1

--- main.py
+++ main.py
@@ -1,50 +1,28 @@
-from turtle import Turtle, Screen
-import random
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
 
 
-is_race_on = False
-screen =Screen()
-screen.setup(width=500, height=400)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Ecter a color: ")
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-color = ["red", "orange", "yellow","green", "blue", "purple"]
-y_point = [100 , 60, 20, -20, -60, -100 ]
-all_turtles = []
-
-for turtle_index in range(0, 6):
-    new_turtle = Turtle(shape ="turtle")
-    new_turtle.color(color[turtle_index])
-    new_turtle.penup()
-    new_turtle.goto(x = -230, y = y_point[turtle_index])
-    all_turtles.append(new_turtle)
-
-if user_bet:
-    is_race_on =True
-
-while is_race_on:
-    for turtle in all_turtles:
-        if turtle.xcor() > 230:
-            is_race_on = False
-            winnig_color = turtle.pencolor()
-            if winnig_color == user_bet:
-                print(f"You've won! The {winnig_color} turtle is the winner!")
-            else:
-                print(f"You've lost! The {winnig_color} turtle is the winner!")
-        rand_distance = random.randint(0,10)
-        turtle.forward(rand_distance)
-
+    snake.move()
 
 
 screen.exitonclick()
-
-
-
-
-# for i, j in zip(color, y_point):
-#     tim = Turtle(shape="turtle")
-#     tim.color(i)
-#     tim.penup()
-#     tim.goto(x=-230, y=j)
-#
-# screen.exitonclick()
